db9e4293632b19d537fa8ffe481d0def2b09ee2f
Luca Antiga <luca.antiga@orobix.com>
2012-11-30 12:02:10 +0100

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to hirop-redis-store
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/hirop_redis_store/core_test.clj
@@ -0,0 +1,7 @@
+(ns hirop-redis-store.core-test
+  (:use clojure.test
+        hirop-redis-store.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
